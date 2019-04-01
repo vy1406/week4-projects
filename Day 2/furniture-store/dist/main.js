8cdd3ca3ba55c16d0247380ec7321f7ba636fd65
Sneguro4ka <yvova88@yahoo.com>
2019-04-01 15:39:06 +0300

    Day 2, exercises 1-6 finished

--- Day 2/furniture-store/dist/main.js
+++ Day 2/furniture-store/dist/main.js
@@ -10,3 +10,19 @@ getFornitureByName = function() {
         $("body").append( `<div>${data.price}`)
     } )
 }
+
+buyFornitureByName = function() {
+    let input = $("#buy-forniture-input").val()
+
+    const url = `/buy/${input}`
+    $.get( url , (data) => {
+        //console.log(data)
+        $("#messages").append( ` <p> Congratulations, you've just bought ${data.name} for ${data.price}. There are ${data.inventory} left now in the store.
+        `)
+    } )
+}
+
+adminReduce = function() {
+    const s = "?admin=true"
+    app.get(`/sale/${s}`)
+}
--- Day 2/furniture-store/server.js
+++ Day 2/furniture-store/server.js
@@ -44,7 +44,82 @@ app.get('/priceCheck/:name', function (request, response) {
 
 // -----------------------------------
 // Exercise 3
-// Add some HTML and client-side JS that allows the user to input the name of a piece of furniture, 
-// then make a GET request to /priceCheck with that input - 
-// you should display the price on the page after it returns.
+/*
+Add some HTML and client-side JS that allows the user to input the name of a piece of furniture, 
+then make a GET request to /priceCheck with that input - 
+you should display the price on the page after it returns.
+*/
 // -----------------------------------
+
+// done.
+
+// -----------------------------------
+// Exercise 4
+/*
+Create another route in your server.js called /buy which has one parameter: name
+Accessing this route should reduce the inventory of that item by 1.
+So if someone makes a request to localhost:3000/buy/chair, it should reduce the 
+inventory of chair to 15.
+*/
+// -----------------------------------
+// app.get('/buy/:name', function (request, response) {
+//     const arg = request.params.name
+//     let curForniture = ""
+//     store.forEach( e => {
+//         if ( e.name == arg){
+//             e.inventory --
+//             curForniture = e
+//         }
+//     })
+//     console.log(store)
+//     response.send(`the ${curForniture.name} inventory's is now ${curForniture.inventory}`)
+// })
+
+// -----------------------------------
+// Exercise 5
+/*
+Add another input and button in your HTML to allow the user to buy something. 
+Again, make a GET request to the /buy route with the user's input.
+After the request is complete, the user should see a message on the page: 
+Congratulations, you've just bought {item} for {price}. There are {inventory} left now in the store.
+*/
+// -----------------------------------
+
+app.get('/buy/:name', function (request, response) {
+    const arg = request.params.name
+    let curForniture = ""
+    store.forEach( e => {
+        if ( e.name == arg){
+            e.inventory --
+            curForniture = e
+        }
+    })
+    response.send(curForniture)
+})
+
+// -----------------------------------
+// Exercise 6
+/*
+Create another route in your server.js called /sale
+This route should accept an optional parameter called admin which takes a boolean
+If the route is accessed with the admin param, 
+it should reduce the price of any item with an inventory greater than 10 by 50%
+So if someone makes a request to localhost:3000/sale/?admin=true, 
+it should change the price of chair to 60, and of picture frame to 35.
+*/
+// -----------------------------------
+
+app.get('/sale', function (request, response) {
+    let params = request.query
+    if ( params.admin == "true"){
+        store.forEach( s => {
+            if ( s.inventory > 10 ) {
+                s.price = s.price / 2
+            }
+        })
+    }
+    else    
+        console.log("else")
+    console.log(store)
+    response.end()
+})
